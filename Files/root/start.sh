b92e6fe9be974ad74a1b74d13477fb3573b99300
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2020-01-01 22:43:08 +0100

    Update start.sh

--- Files/root/start.sh
+++ Files/root/start.sh
@@ -4,4 +4,6 @@ echo "[info] Starting Cronie....."
 /usr/sbin/crond &
 
 echo "[info] Starting Unbound....."
+if [[ ! -z "LISTEN" ]]; then
+sed -i "s/interface:.*$/interface: ${LISTEN}/" /etc/unbound/unbound.conf
 /usr/sbin/unbound
