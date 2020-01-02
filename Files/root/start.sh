9e3fa59e821d415ee172ada0dfdaaa324d06b528
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2020-01-02 11:55:33 +0100

    Update start.sh
    Fix Permissions

--- Files/root/start.sh
+++ Files/root/start.sh
@@ -8,6 +8,7 @@ echo "[info] Changing Interface to: $LISTEN"
 sed -i "1,/interface/ s/interface: .*$/interface: $LISTEN/" /etc/unbound/unbound.conf
 fi
 echo "[info] Starting Unbound....."
+chown -R unbound:unbound /etc/unbound
 /usr/sbin/unbound -d -vvv
 echo "[info] Starting Bash for Error searching....."
 /bin/sh
