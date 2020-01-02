7a40648f307356fe78845e3e516d1c8b384b2c18
mmtnrw <46136708+mmtnrw@users.noreply.github.com>
2020-01-02 10:48:23 +0100

    Update start.sh

--- Files/root/start.sh
+++ Files/root/start.sh
@@ -3,10 +3,11 @@
 echo "[info] Starting Cronie....."
 /usr/sbin/crond &
 
-echo "[info] Starting Unbound....."
 if [[ ! -z "$LISTEN" ]]; then
-sed -i -e "0,/interface: .*$/s//interface: ${LISTEN}/1" /etc/unbound/unbound.conf
+echo "[info] Changing Interface to: $LISTEN"
+sed -i "1,/interface/ s/interface: .*$/interface: $LISTEN/" /etc/unbound/unbound.conf
 fi
+echo "[info] Starting Unbound....."
 /usr/sbin/unbound -d -vvv
 echo "[info] Starting Bash for Error searching....."
 /bin/sh
